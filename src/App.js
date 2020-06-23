26937d662eb079bc72f7e91a11abd4aae29ec96f
Erika <erika.paige.handley@gmail.com>
2020-06-22 18:07:19 -0700

    updated employee array

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// bring in React and 'component' constructor to use smart components
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  
+  state = {
+    // setting this.state.employees to employees json array
+    employees
+    //properties on JSON
+  }
+
+  searchEmployeeFirstName = first_name =>{
+    
+  }
+  render() {
+    return (
+      
+    )
+  }
 }
 
 export default App;
